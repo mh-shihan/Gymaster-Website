da7099c9c4e0aee5d2e9648a45c27a2a417ff241
MHShihan <mahmodul.no.1@gmail.com>
2023-10-06 22:11:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Vite + React</h1>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
